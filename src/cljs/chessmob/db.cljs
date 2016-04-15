ede9bb3d4ac1df324a6b0e1fd8cc5f97a4e76b15
Liam Dyer <liamdyer@Bills-MacBook-Pro.local>
2016-04-15 01:32:02 -0500

    Draws board with text labels for pieces

--- src/cljs/chessmob/db.cljs
+++ src/cljs/chessmob/db.cljs
@@ -1,5 +1,22 @@
 (ns chessmob.db)
 
+(def default-board
+  [[{:type 'rook :color 'black} {:type 'knight :color 'black}
+    {:type 'bishop :color 'black} {:type 'queen :color 'black}
+    {:type 'king :color 'black} {:type 'bishop :color 'black}
+    {:type 'knight :color 'black} {:type 'rook :color 'black}]
+   (into [] (repeat 8 {:type 'pawn :color 'black}))
+   (into [] (repeat 8 {:type 'empty-space}))
+   (into [] (repeat 8 {:type 'empty-space}))
+   (into [] (repeat 8 {:type 'empty-space}))
+   (into [] (repeat 8 {:type 'empty-space}))
+   (into [] (repeat 8 {:type 'pawn :color 'white}))
+   [{:type 'rook :color 'white} {:type 'knight :color 'white}
+    {:type 'bishop :color 'white} {:type 'king :color 'white}
+    {:type 'queen :color 'white} {:type 'bishop :color 'white}
+    {:type 'knight :color 'white} {:type 'rook :color 'white}]])
+
 (def default-db
-  {:name "Dale"})
+  {:name "Dale"
+   :board default-board})
 
--- src/cljs/chessmob/views.cljs
+++ src/cljs/chessmob/views.cljs
@@ -1,7 +1,19 @@
 (ns chessmob.views
     (:require [re-frame.core :as re-frame]))
 
+(defn tile-view [color tile]
+  [:div {:style {:backgroundColor color :display "inline-block" 
+                 :height "40px" :width "40px"}}
+        (str (:type tile))])
+
+(defn row-view [first-color row]
+  [:div {:style {:height "40px"}} (map tile-view 
+                                       (cycle (if (= first-color "white")
+                                                  ["white" "black"]
+                                                  ["black" "white"])) 
+                                       row)])
+
 (defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
+  (let [board (re-frame/subscribe [:board])]
     (fn []
-      [:h1 "Hello from " @name])))
+      [:div (map row-view (cycle ["white" "black"]) @board)])))
